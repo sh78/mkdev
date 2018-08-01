5f0e7a8d97cecfb960ba8934a9199ed570756b31
Sean Henderson <mail@sean.sh>
2018-07-31 21:41:41 -0700

    Don't save session files for now.
    Too confusing/non-standard for it to remember between invocations.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const shell = require('shelljs')
 const prompt = require('./lib/prompt')
 const chalk = require('chalk')
-// const red = chalk.red
+const red = chalk.red
 const yellow = chalk.yellow
 // const green = chalk.green
 const log = console.log
@@ -20,29 +20,23 @@ log(conf)
 const system = require('./lib/system.js')
 log(system)
 
-const session = require('./lib/session.js')
-log(session)
-
 if (system.diskFreeIsGb && system.diskFreeNum > 10) {
-  log(yellow(`You have ${system.diskFree} of free disk space.`))
-  if (!session.diskLowOverride) {
-    log(yellow(`Are you sure you want to continue the install?`))
-    prompt('(y/n): ')
-      .then((answer) => {
-        answer = answer.toLowerCase()
-        if (answer === 'y' || answer === 'yes') {
-          log('Ok, proceeding...')
-          fs.writeFile('session/diskLowOverride', '', function (err) {
-            if (err) return log(err)
-          })
-        } else {
-          process.exit()
-        }
-      })
-      .catch((error) => {
-        log("There's an error!")
-        log(error)
+  log(yellow(
+    `You have ${system.diskFree} of free disk space.` + '\n' +
+    `Are you sure you want to continue the install?`
+  ))
+  prompt('(y/n): ')
+    .then((answer) => {
+      answer = answer.toLowerCase()
+      if (answer === 'y' || answer === 'yes') {
+        log('Ok, proceeding...')
+      } else {
         process.exit()
-      })
-  }
+      }
+    })
+    .catch((error) => {
+      log(red("There's an error!"))
+      log(error)
+      process.exit()
+    })
 }
