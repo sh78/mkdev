351c62073a6df1517f4c268f2f3c96b871c5756d
Sean Henderson <mail@sean.sh>
2018-07-31 21:12:45 -0700

    Make session info into a module and use for disk check.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 // const yaml = require('js-yaml')
-// const shell = require('shelljs')
+const shell = require('shelljs')
 const prompt = require('./lib/prompt')
 const chalk = require('chalk')
 // const red = chalk.red
@@ -20,26 +20,29 @@ log(conf)
 const system = require('./lib/system.js')
 log(system)
 
-if (system.diskFreeIsGb && system.diskFreeNum > 5) {
-  log(yellow(
-    `You have ${system.diskFree} of free disk space.` + '\n' +
-    `Are you sure you want to continue the install?`
-  ))
-  // TODO: if file session/diskLowOverride ! exists
-  prompt('(y/n): ')
-    .then((answer) => {
-      if (answer === 'y' || answer === 'yes') {
-        log('Ok, proceeding...')
-        fs.writeFile('session/diskLowOverride', '', function (err) {
-          if (err) return log(err)
-        })
-      } else {
+const session = require('./lib/session.js')
+log(session)
+
+if (system.diskFreeIsGb && system.diskFreeNum > 10) {
+  log(yellow(`You have ${system.diskFree} of free disk space.`))
+  if (!session.diskLowOverride) {
+    log(yellow(`Are you sure you want to continue the install?`))
+    prompt('(y/n): ')
+      .then((answer) => {
+        answer = answer.toLowerCase()
+        if (answer === 'y' || answer === 'yes') {
+          log('Ok, proceeding...')
+          fs.writeFile('session/diskLowOverride', '', function (err) {
+            if (err) return log(err)
+          })
+        } else {
+          process.exit()
+        }
+      })
+      .catch((error) => {
+        log("There's an error!")
+        log(error)
         process.exit()
-      }
-    })
-    .catch((error) => {
-      log("There's an error!")
-      log(error)
-      process.exit()
-    })
+      })
+  }
 }
